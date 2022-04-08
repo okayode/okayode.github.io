aa2a0745eb5533ecd8dfa0325c21963c47ea1fea
Kayode Olumoyin <kayodaniel01@gmail.com>
2022-04-07 19:08:15 -0500

    Update estimating_pi_using monteCarlo.py

--- estimating_pi_using monteCarlo.py
+++ estimating_pi_using monteCarlo.py
@@ -1,35 +1,41 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-INTERVAL= 1000
+N = 100000
 
-circle_points= 0
-square_points= 0
+circle_points = 0
+square_points = 0
+inside_circle_x= [] 
+inside_circle_y= [] 
+outside_circle_x= [] 
+outside_circle_y= [] 
 
-# Total Random numbers generated= possible x
-# values* possible y values
-for i in range(INTERVAL**2):
+for i in range(N):
+    rand_x = random.uniform(-1, 1)
+    rand_y = random.uniform(-1, 1)
+    radius = rand_x**2 + rand_y**2
+    
+    # Checking if (x, y) lies inside the circle
+    if radius<= 1:
+      circle_points+= 1
+      inside_circle_x.append(rand_x)
+      inside_circle_y.append(rand_y)
 
-	# Randomly generated x and y values from a
-	# uniform distribution
-	# Range of x and y values is -1 to 1
-	rand_x= random.uniform(-1, 1)
-	rand_y= random.uniform(-1, 1)
+    elif radius> 1:
+      outside_circle_x.append(rand_x)
+      outside_circle_y.append(rand_y)
+      
+    square_points+= 1
+    
+    # Estimating value of pi,
+    # pi= 4*(no. of points generated inside the
+    # circle)/ (no. of points generated inside the square)
+    pi = 4* circle_points/ square_points
 
-	# Distance between (x, y) from the origin
-	origin_dist= rand_x**2 + rand_y**2
+print("Estimation of Pi=", pi)
 
-	# Checking if (x, y) lies inside the circle
-	if origin_dist<= 1:
-		circle_points+= 1
+plt.scatter(inside_circle_x,inside_circle_y, c='b', alpha=0.5, edgecolor=None)
+plt.scatter(outside_circle_x,outside_circle_y, c='r', alpha=0.5, edgecolor=None)
 
-	square_points+= 1
-
-	# Estimating value of pi,
-	# pi= 4*(no. of points generated inside the
-	# circle)/ (no. of points generated inside the square)
-	pi = 4* circle_points/ square_points
-
-## print(rand_x, rand_y, circle_points, square_points, "-", pi)
-## print("\n")
-
-print("Final Estimation of Pi=", pi)	
+plt.show()
